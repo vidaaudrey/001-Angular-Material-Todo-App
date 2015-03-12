4d5ed2e965b30e9f84b497b809286ffbc80c2a45
vidaaudrey <audrey@33road.com>
2015-03-12 16:05:36 -0700

    basic todo function'

---
+++ app/src/todos/TodoController.js
@@ -0,0 +1,44 @@
+angular.module('todoApp', ['ngMaterial'])
+  .controller('TodoController', ['$scope', function($scope) {
+    $scope.todos = [
+      {text:'learn angular', done:true},
+      {text:'build an angular app', done:false}];
+ 
+    $scope.addTodo = function() {
+      $scope.todos.push({text:$scope.todoText, done:false});
+      $scope.todoText = '';
+    };
+ 
+    $scope.remaining = function() {
+      var count = 0;
+      angular.forEach($scope.todos, function(todo) {
+        count += todo.done ? 0 : 1;
+      });
+      return count;
+    };
+ 
+    $scope.archive = function() {
+      var oldTodos = $scope.todos;
+      $scope.todos = [];
+      angular.forEach(oldTodos, function(todo) {
+        if (!todo.done) $scope.todos.push(todo);
+      });
+    };
+  }])
+  .config(function($mdThemingProvider, $mdIconProvider){
+
+      $mdIconProvider
+          .defaultIconSet("./assets/svg/avatars.svg", 128)
+          .icon("menu"       , "./assets/svg/menu.svg"        , 24)
+          .icon("add"       , "./assets/svg/add.svg"        , 48)
+          .icon("share"      , "./assets/svg/share.svg"       , 24)
+          .icon("google_plus", "./assets/svg/google_plus.svg" , 512)
+          .icon("hangouts"   , "./assets/svg/hangouts.svg"    , 512)
+          .icon("twitter"    , "./assets/svg/twitter.svg"     , 512)
+          .icon("phone"      , "./assets/svg/phone.svg"       , 512);
+
+          $mdThemingProvider.theme('default')
+              .primaryPalette('blue')
+              .accentPalette('red');
+
+    });
